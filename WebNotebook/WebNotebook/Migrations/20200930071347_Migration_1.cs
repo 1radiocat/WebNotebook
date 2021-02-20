c34c202daf4d60de964e2c5243406b338382980d
1radiocat <67992159+1radiocat@users.noreply.github.com>
2021-02-20 11:44:52 +0700

    new build

---
+++ WebNotebook/WebNotebook/Migrations/20200930071347_Migration_1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebNotebook.Migrations
+{
+    public partial class Migration_1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Records",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: false),
+                    Phone = table.Column<string>(maxLength: 20, nullable: true),
+                    Email = table.Column<string>(maxLength: 50, nullable: true),
+                    BirthDate = table.Column<DateTime>(nullable: false),
+                    Company = table.Column<string>(maxLength: 200, nullable: true),
+                    Comment = table.Column<string>(maxLength: 200, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Records", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Records");
+        }
+    }
+}
