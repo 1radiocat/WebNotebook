a93f5eb254047333efc4966a463cb1d8fa0d32dc
1radiocat <67992159+1radiocat@users.noreply.github.com>
2021-02-22 17:12:38 +0700

    Update

---
+++ WebNotebook/VueJS/vue-page.vue
@@ -0,0 +1,11 @@
+﻿<template>
+    <div id="divVuePage" class="divVuePage">
+        <h1>Hello</h1>
+    </div>
+</template>
+
+<script lang="ts" src="https://cdn.jsdelivr.net/npm/vue@2.6.11/dist/vue.js">
+</script>
+
+<style lang="scss" src="./vue-page.scss"></style>
+<i18n src="./vue-page.i18n"></i18n>
