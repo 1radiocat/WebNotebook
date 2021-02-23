859435c9850754daec3d50b1d745ee6f229f6e86
1radiocat <67992159+1radiocat@users.noreply.github.com>
2021-02-23 21:07:36 +0700

    + vue.js

---
+++ WebNotebook/Scripts/VuePage/index.js
@@ -0,0 +1,9 @@
+﻿import Vue from 'vue';
+import VuePageComponent from './vue-page.vue';
+
+new Vue({
+    el: "#app",
+    components: {
+        VuePageComponent
+    }
+})
---
+++ WebNotebook/Scripts/VuePage/vue-page.vue
@@ -0,0 +1,15 @@
+﻿<template>
+    <div id="vuepage">
+        <h1>Hello VUE</h1>
+    </div>
+</template>
+
+
+<script>
+    export default {
+        name: "vue-component"
+    };
+</script>
+
+<!--<style lang="scss" src="./vue-page.scss"></style>
+<i18n src="./vue-page.i18n"></i18n>-->
